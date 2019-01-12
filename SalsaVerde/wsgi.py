0b3da4729f0d261cf5606d0b2af68f3fc8e4387d
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2019-01-12 15:05:40 +0000

    Fix whitenoise

--- SalsaVerde/wsgi.py
+++ SalsaVerde/wsgi.py
@@ -3,7 +3,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SalsaVerde.settings")
 
 from django.core.wsgi import get_wsgi_application  # noqa
-from whitenoise.django import DjangoWhiteNoise  # noqa
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
