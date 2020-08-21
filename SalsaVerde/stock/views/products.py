fcb18446bad1a360d8a5470368b1812210993731
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2020-08-21 14:26:49 +0100

    Add finished product view

--- SalsaVerde/stock/views/products.py
+++ SalsaVerde/stock/views/products.py
@@ -139,13 +139,19 @@ class ProductList(ListView):
     order_by = '-date_of_bottling'
     paginate_by = 50
 
+    def dispatch(self, request, *args, **kwargs):
+        self.view_finished = bool(self.request.GET.get('finished'))
+        return super().dispatch(request, *args, **kwargs)
+
     def get_queryset(self):
-        return super().get_queryset().select_related('product_type')
+        return super().get_queryset().select_related('product_type').filter(finished=self.view_finished)
 
     def get_button_menu(self):
-        return [
-            {'name': 'Record new product infusion', 'url': reverse('products-add')},
-        ]
+        yield {'name': 'Record new product infusion', 'url': reverse('products-add')}
+        if self.view_finished:
+            yield {'name': 'View Current Products', 'url': reverse('products')}
+        else:
+            yield {'name': 'View Finished Products', 'url': reverse('products') + '?finished=true'}
 
 
 product_list = ProductList.as_view()
